a04157f816049f6c8febe2d419dd62470b7d7ed6
yasmin <yasmin075souza@hotmail.com>
2024-09-04 15:24:05 -0300

    desafio 5 -feito

---
+++ desafio-5.py
@@ -0,0 +1,6 @@
+# Escreva um programa que inverta os caracteres de um string.
+string = input("Digite uma palavra que você deseja inverter: ")
+
+string_invertida = string[::-1]
+
+print(f"String Invertida: {string_invertida}")
